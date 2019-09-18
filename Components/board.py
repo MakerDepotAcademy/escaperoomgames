4337660020dada25cbc86681885dd32796c2de43
Dan Habot <dan.habot@gmail.com>
2019-09-18 17:25:53 -0400

    Refactored all game management

--- Components/board.py
+++ Components/board.py
@@ -4,93 +4,16 @@ from threading import Thread, Lock
 import re
 from random import randint
 from time import sleep
-import components.settings as Settings
-
-DEV = False
-DEV_COUNT = 2
-DEV_BIDS = iter(Settings.BoardStack().Board_Stack)
-
-class DevSerial():
-  
-  def __init__(self, port, baudrate=2000000, timeout=1):
-    self._port = port
-    self._br = baudrate
-    self._timeout = timeout
-    self._status = ['x'] * 32
-    self._pins = ['0'] * 32
-    self._read_stack = []
-    self._id = next(DEV_BIDS)
-    self.closed = False
-
-  def write(self, d):
-    if self.closed:
-      raise Exception('Serial is closed')
-
-    d = [chr(i) for i in d]
-    if len(d) >= 32:
-      for i in range(32):
-        if d[i] != 'x':
-          self._status[i] = d[i]
-      return
-
-    if d[0] == '?':
-      self._read_stack.append(self._id)
-      return
-
-    if d[0] == 'l':
-      return
-
-    if d[0] == 'v':
-      self._read_stack.append('dev')
-
-    if d[0] == 'r':
-      self._read_stack.append(self._pins)
-      return
-
-    if d[0] == 'R':
-      self._read_stack.append(self._status)
-      return
-
-    if d[0] == 's':
-      self._status[d[1:-1]] = d[-1]
-      return
-
-  def readline(self):
-    if self._read_stack == []:
-      sleep(self._timeout)
-      return ''
-    else:
-      return str(self._read_stack.pop()).encode()
-
-  def close(self):
-    self.close = True
-
-  def flushInput(self):
-    pass
-
-  def flushOutput(self):
-    pass
-
-  def interrupt(self, pin, val=-1):
-    buff = self._status
-    buff[pin] = not buff[pin] if val == -1 else val
-    self._read_stack.append(buff)
 
 
 class Board():
   
   def __init__(self, port, qu=32, id=None):
-    if DEV:
-      self._ser = DevSerial(port)
-    else:
-      self._ser = serial.Serial(str(port), 2000000, timeout=1)
+    self._ser = serial.Serial(str(port), 2000000, timeout=1)
     self.flush()
     self._hooks = []
     self.queue = ['x'] * qu
     self._queuelen = qu
-    self._eventThread = Thread(target=self._eventLoop)
-    self._eventAlive = False
-    self._eventLock = Lock()
     self._id = id
 
   def close(self):
@@ -99,24 +22,6 @@ class Board():
   def flush(self):
     self._ser.flushInput()
     self._ser.flushOutput()
-
-  def _eventLoop(self):
-    last = ''
-    while True:
-      self._eventLock.acquire(True)
-      line = self._ser.readline()
-      self._eventLock.release()
-
-      if len(line) == 0 or last == line:
-        continue
-      
-      for i, (c, l) in enumerate(zip(line, last)):
-        if c != l:
-          for hook, pin in self._hooks:
-            if pin == -1 or pin == i + 1:
-              hook(pin, c == '1')
-
-      last = line
 
   def run(self):
     t = ''.join(self.queue) + '\n'
@@ -130,7 +35,6 @@ class Board():
     r('U')
     r('I')
     r('0')
-    
 
   def _setpin(self, pin, val):
     self.queue[pin] = val
@@ -166,33 +70,6 @@ class Board():
   
   def getPorts(self):
     return self._prompt('r')
-
-  def onChange(self, hook, pin=-1):
-    self._hooks.append((hook, pin))
-    
-    if pin == -1:
-      self.queue = 'e' * self._queuelen
-    else:
-      self.queue[pin - 1] = 'e'
-
-    self.run()
-
-    if not self._eventAlive:
-      self._eventThread.start()
-      self._eventAlive = True
-
-  def clearHooks(self):
-    self._hooks = []
-    self.queue = 'd' * self._queuelen
-    self.run()
-
-  def clearHook(self, pin):
-    for i, ent in enumerate(self._hooks):
-      if ent[1] == pin:
-        del self._hooks[i]
-        self.queue[pin - 1] == 'd'
-
-    self.run()
 
   def awaitChange(self, pins, timeout, timeout_tick=None):
     def m(l):
@@ -240,23 +117,25 @@ class Board():
 
 class Manager():
   
-  def __init__(self):
-    self._boards = {}
-    l = None
-    if DEV:
-      l = range(len(Settings.BoardStack().Board_Stack))
-    else:
-      l = Path('/dev').glob('ttyACM*')
+  def __init__(self, order):
+    self._boards = []
+    t = {}
     
-    for acm in l:
+    for acm in Path('/dev').glob('ttyACM*'):
       b = Board(acm)
       for a in range(5): # For 5 attempts
-        i = b.getID()
-        if re.search(r'[0-9]+', i):
-          self._boards[int(i)] = b
-          print('Got board %s at %s' % (i, str(acm)))
-          break
-  
+        try:
+          i = int(b.getID())
+        except ValueError:
+          continue
+        else:
+          if re.search(r'[0-9]+', i):
+            t[int(i)] = b
+            print('Got board %s at %s' % (i, str(acm)))
+            break
+
+    for o in order:
+      self._boards.append(t[o])
 
   def __getitem__(self, i):
     return self.getBoardByID(i)
@@ -270,3 +149,7 @@ class Manager():
   def closeall(self):
     for k in self._boards:
       self._boards[k].close()
+
+  def resetall(self):
+    for k in self._boards:
+      self._boards[k].reset()
---
+++ Components/game.py
@@ -0,0 +1,154 @@
+import toml
+
+from flask import Flask, request
+from abc import ABC, abstractmethod
+from time import sleep
+from threading import Thread
+
+from pause import Pause
+from board import Manager
+
+class GameKilled(Exception):
+  pass
+
+class Game(ABC):
+
+  def __init__(self, configpath):
+    self._api = Flask(__name__)
+    self._pause = Pause(self.pause_change)
+    self._config = toml.load(configpath)
+    # self._manager = Manager(self.get_config('BOARD', 'STACK').split(','))
+    self._manager = []
+    self._dump = {}
+    self._alive = True
+    self._lifespan = self.get_config('TIME', 'GAME_TIME', type=int, default=300)
+    self._playing = False
+    self._roundtime = self.get_config('TIME', 'ROUND_TIME', type=int, default=10)
+
+  @abstractmethod
+  def gameLogic(self):
+    pass
+
+  @abstractmethod
+  def pause_change(self, paused):
+    pass
+
+  @abstractmethod
+  def game_tick(self, time):
+    pass
+
+  @abstractmethod
+  def round_tick(self, time):
+    pass
+
+  @abstractmethod
+  def once_killed(self):
+    pass
+
+  def get_config(self, scope, arg, **kwargs):
+    default = kwargs.pop('default', None)
+    _type = kwargs.pop('type', str)
+
+    if default is not None and type(default) != _type:
+      raise TypeError('default= must be %s' % type(_type))
+    
+    t = self._config[scope]
+    try:
+      return _type(t[arg])
+    except KeyError as e:
+      if default is None:
+        raise e
+      else:
+        return _type(default)
+
+  def kill(self, force=False):
+    self._alive = False
+    if force:
+      self.killed()
+
+  def killed(self):
+    if self._alive == False:
+      for b in self._manager:
+        b.resetall()
+        b.closeall()
+      self.once_killed()
+      raise GameKilled('Game has been killed')
+
+  def block(self):
+    self.killed()
+    self._pause.block_if_paused()
+
+  def sleep(self, t):
+    i = t
+    while i > 0:
+      self.block()
+      i -= 1
+      sleep(1)
+
+  def _gameLoop(self):
+    self.gameLogic()
+    self.kill(True)
+
+  def _gameTimer(self):
+    # This will nuke threads too, thanks brad
+    i = self._lifespan
+    while i > 0 and self._playing:
+      self.sleep(1)
+      i -= 1
+      self.game_tick(i)
+      if i == 0:
+        # Kill game
+        self.kill(True)
+
+  def _roundTimer(self):
+    i = self._roundtime
+    while i > 0 and self._playing:
+      self._pause.block_if_paused()
+      self.round_tick(i)
+      sleep(1)
+      i -= 1
+    self.round_tick(-1)
+    self._playing = False
+      
+
+  def startRound(self):
+    t = Thread(target=self._roundTimer)
+    t.start()
+
+  def stopRound(self):
+    self._playing = False
+
+  def serve(self, port=5000):
+    @self._api.route('/start', methods=['POST'])
+    def flask_start_game():
+      self._playing = True
+      self._alive = True
+      self._gameloopthread = Thread(target=self._gameLoop)
+      self._gametimerthread = Thread(target=self._gameTimer)
+      self._gameloopthread.start()
+      self._gametimerthread.start()
+
+      return 'started'
+
+    @self._api.route('/pause')
+    def flask_pause_game():
+      self._pause.pause()
+      if self._pause.isPaused():
+        return 'Game is paused'
+      else:
+        return 'Game is running'
+
+    @self._api.route('/score')
+    def flask_get_score():
+      return str(Scores.score)
+
+    @self._api.route('/dump')
+    def flask_dump():
+      return self._dump
+
+    @self._api.route('/kill')
+    def flask_kill():
+      self.kill()
+      return 'killed'
+
+    self._api.run(host='0.0.0.0', port=port)
