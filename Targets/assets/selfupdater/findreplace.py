c09c2af02fde4a45d7cc1e917da1c9f514981bf1
Dan Habot <dan.habot@gmail.com>
2019-09-29 00:12:34 -0400

    Update find replace

--- Targets/assets/selfupdater/findreplace.py
+++ Targets/assets/selfupdater/findreplace.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-import sys, re
+import sys, re, os
 with open(sys.argv[1], 'r') as f:
   s = re.sub(r'REPLACEME', sys.argv[2], f.read())
+  s = re.sub(r'REPLACEDIR', os.path.dirname(sys.argv[2]), s)
 with open(sys.argv[1], 'w') as f:
   f.write(s)
