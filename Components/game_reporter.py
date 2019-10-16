9a38e7a813aa429fe8bdfad0bf0586aef473b1cf
Brad Totaro <excalibursafetycaptain@gmail.com>
2019-10-15 21:40:31 -0400

    Remove franks name

--- Components/game_reporter.py
+++ Components/game_reporter.py
@@ -8,7 +8,7 @@ def report_score(teamID,teamName,score):
     requests.post("http://192.168.1.20:3030/game-complete",
       data={'teamId':teamID,
             "score":score,
-            "teamName":"Frank2"
+            "teamName":teamName
            } #data
     ).content #req post
     )#debug
